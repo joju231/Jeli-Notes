b429aceddef1b72b81b91fe559e9296dac4bfacf
joju231 <86151434+joju231@users.noreply.github.com>
2024-06-01 16:03:19 +0200

    Add files via upload

---
+++ php/conections.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    $conection = mysqli_connect("localhost", "root", "", "jeli_labs_login-register");
+    
+    /*
+    if($conection){
+        echo 'conectado correctamente';
+    }else{
+        echo 'no se ha podido conectar';
+    }
+    */
+
+
+?>
